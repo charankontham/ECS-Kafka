0bddca4199392272de156d4d22097f7146697304
= <=>
2024-12-31 14:29:35 -0500

    initial push from local repo

---
+++ src/main/java/com/ecs/ecs_kafka/EcsKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.ecs.ecs_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecs/ecs_kafka/EcsKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecs.ecs_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
